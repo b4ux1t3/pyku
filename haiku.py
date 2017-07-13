062d205fa96793b53de3179770dd90558da72f37
Chris Pilcher <christopher.a.pilcher@gmail.com>
2017-07-12 21:42:58 -0400

    Initial commit

---
+++ haiku.py
@@ -0,0 +1,33 @@
+import random
+fives = open("five.txt").read().splitlines()
+sevens = open("seven.txt").read().splitlines()
+def main():
+	firstFive = getFirstFive()
+	secondFive = getSecondFive(firstFive)
+	seven = random.choice(sevens)
+	
+	output = firstFive + "\n" + seven + "\n" + secondFive
+	
+	print output
+	f = open("haiku.txt", "w")
+	f.write(output)
+	f.close()
+	
+def getFirstFive():
+	firstFive = random.choice(fives)
+	if firstFive == "":
+		firstFive = getFirstFive()
+		return(firstFive)
+	else:
+		return firstFive
+		
+def getSecondFive(firstFive):
+	secondFive = random.choice(fives)
+	if secondFive == firstFive or secondFive == "":
+		secondFive = getSecondFive(firstFive)
+		return secondFive
+	else:
+		return secondFive
+	
+if __name__ == "__main__":
+	main()
